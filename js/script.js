d0185eb06dd7c20bc76310cb0b47786e182d776e
Verta-master <vertyagina@gmail.com>
2018-07-18 16:51:08 +0600

    Blur

--- js/script.js
+++ js/script.js
@@ -16,4 +16,56 @@ $(window).scroll(function(){
   };
 });
 
+//Range slider
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 15,
+    max: 55,
+    values: [ 24, 40 ],
+    slide: function( event, ui ) {
+      $( "#amount-min" ).val( ui.values[ 0 ] + ' сек.');
+      $( "#amount-max" ).val( ui.values[ 1 ] + ' сек.');
+    }
+  });
+  $( "#amount-min" ).val( $( "#slider-range" ).slider( "values", 0 ) + ' сек.');
+  $( "#amount-max" ).val( $( "#slider-range" ).slider( "values", 1 ) + ' сек.');
+});
+  
+//Show & hide brief
+$('#price').click(function(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  $(".main").addClass('main--brief');
+  $('.brief').addClass('brief--show');
+})
+  
+$("body").click(function(){
+  $('.brief').removeClass('brief--show');
+  $(".main").removeClass('main--brief');
+});
+
+$("body").keydown(function(evt) {
+  if (evt.keyCode == 27) {
+    $('.brief').removeClass('brief--show');
+    $(".main").removeClass('main--brief');
+  }
+});
+
+//Show & hide main iframe
+  $('.promo__play').click(function(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  $('.promo__iframe').addClass('promo__iframe--show');
+})
+  
+$("body").click(function(){
+  $('.promo__iframe').removeClass('promo__iframe--show');
+});
+
+$("body").keydown(function(evt) {
+  if (evt.keyCode == 27) {
+    $('.promo__iframe').removeClass('promo__iframe--show');
+  }
+});
 });
